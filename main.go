4f326a499cda8fad89d5158dacaf4c53ff737b0c
reidwids <reidwids@gmail.com>
2024-02-05 00:44:00 -0500

    init blockchain v2

--- main.go
+++ main.go
@@ -1,12 +1,28 @@
 package main
 
 import (
-	"GoBlockchain/internal/api"
+	"GoBlockchain/internal/blockchain"
+	"fmt"
+	"strconv"
 )
 
 func main() {
 	// Define command-line flags
-	api.Start()
+	// api.Start()
+	chain := blockchain.InitBlockChain()
+	chain.AddBlock("First Block")
+	chain.AddBlock("Second Block")
+	chain.AddBlock("Third Block")
+
+	for _, block := range chain.Blocks {
+		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
+		fmt.Printf("Data in Block: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+
+		pow := blockchain.NewProof(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool((pow.Validate())))
+		fmt.Println()
+	}
 
 	// createBlockchain := flag.Bool("create", false, "Create a new blockchain")
 	// addBlock := flag.String("addblock", "", "Add a new block to the blockchain")
