2ffdfcacd314b4509fbd3a63de0b5d0632b03ee8
reidwids <reidwids@gmail.com>
2024-02-07 00:41:07 -0500

    Document blockchain

--- internal/blockchain/block.go
+++ internal/blockchain/block.go
@@ -14,10 +14,14 @@ type Block struct {
 }
 
 func CreateBlock(data string, prevHash []byte) *Block {
+	// Create an empty block with the prev hash
 	block := &Block{[]byte{}, []byte(data), prevHash, 0}
+
+	// Create a new pow instance from the new block and run the proof
 	pow := NewProof(block)
 	nonce, hash := pow.Run()
 
+	// set the hash and nonce
 	block.Hash = hash[:]
 	fmt.Printf("Hash: %x\n", block.Hash)
 	block.Nonce = nonce
@@ -26,15 +30,20 @@ func CreateBlock(data string, prevHash []byte) *Block {
 }
 
 func (chain *BlockChain) AddBlock(data string) {
+	// Create a new block with the given data and the previous hash
 	prevBlock := chain.Blocks[len(chain.Blocks)-1]
 	new := CreateBlock(data, prevBlock.Hash)
+
+	// Append the new block to the chain
 	chain.Blocks = append(chain.Blocks, new)
 }
 
 func Genesis() *Block {
+	// Init the chain with the genesis block
 	return CreateBlock("Genesis", []byte{})
 }
 
 func InitBlockChain() *BlockChain {
+	// Create a new blockchain with an instance of a genesis block
 	return &BlockChain{[]*Block{Genesis()}}
 }
--- internal/blockchain/proof.go
+++ internal/blockchain/proof.go
@@ -18,6 +18,9 @@ type ProofOfWork struct {
 }
 
 func NewProof(b *Block) *ProofOfWork {
+	// By left shifting 1 by (256 - Difficulty), we get a target that is missing the first n bits
+	// This will be the target for the proof of work, where we will try to find a hash that is less
+	// than the target - meaning it has the first n bits set to 0
 	target := big.NewInt(1)
 	target.Lsh(target, uint(256-Difficulty))
 
@@ -31,6 +34,7 @@ func (pow *ProofOfWork) InitData(nonce int) []byte {
 		[][]byte{
 			pow.Block.PrevHash,
 			pow.Block.Data,
+			// Could maybe take out the ToHex function in the future
 			ToHex(int64(nonce)),
 			ToHex(int64(Difficulty)),
 		},
@@ -40,37 +44,46 @@ func (pow *ProofOfWork) InitData(nonce int) []byte {
 }
 
 func (pow *ProofOfWork) Validate() bool {
+	// Validate that a proof of work meets the target requirements
 	var intHash big.Int
+	// Create a new data slice with the provided nonce
 	data := pow.InitData(pow.Block.Nonce)
 
+	// Create a new sha256 hash from the data
 	hash := sha256.Sum256(data)
 	intHash.SetBytes(hash[:])
 
+	// Check that the hash from the given data is less than the target
 	return intHash.Cmp(pow.Target) == -1
 }
 
 func ToHex(num int64) []byte {
+	// Use a buffer to write the binary data to
 	buff := new(bytes.Buffer)
 	err := binary.Write(buff, binary.BigEndian, num)
 	if err != nil {
 		log.Panic(err)
 	}
+	// Return the bytes from the buffer
 	return buff.Bytes()
 }
 
 func (pow *ProofOfWork) Run() (int, []byte) {
+
 	var intHash big.Int
 	var hash [32]byte
-
 	nonce := 0
 
+	// Increment the nonce until we find a hash that is less than the target
 	for nonce < math.MaxInt64 {
 		data := pow.InitData(nonce)
 		hash = sha256.Sum256((data))
 		fmt.Printf("\r%x", hash)
 
+		// Set the hash to the big.Int
 		intHash.SetBytes(hash[:])
 
+		// Check if the hash is less than the target
 		if intHash.Cmp(pow.Target) == -1 {
 			break
 		} else {
